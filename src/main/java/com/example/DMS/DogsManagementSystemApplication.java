38ebc1c445672df3a0392cda3e0393ffe090e54b
chandanaaluri <chandanaaluri05@gmail.com>
2025-04-24 15:31:10 -0500

    added my project

---
+++ src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * @author Chandana Aluri
+ */
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
