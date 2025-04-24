5c59cce0049eee47cce897b25cd17828ebf2cf25
Chandana aluri <chandanaaluri05@gmail.com>
2025-04-24 16:51:04 -0500

    Update DogController.java

--- src/main/java/com/example/DMS/Controllers/DogController.java
+++ src/main/java/com/example/DMS/Controllers/DogController.java
@@ -1,98 +1,102 @@
 package com.example.DMS.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.example.DMS.Models.Dog;
 import com.example.DMS.Models.Trainer;
 import com.example.DMS.repository.DogRepository;
 import com.example.DMS.repository.TrainerRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class DogController {
 
-    // Using a single shared ModelAndView is not thread-safe; creating a new instance for each method is better.
     @Autowired
-    DogRepository dogRepo;
-    @Autowired
-    TrainerRepository trainerRepo;
+    private DogRepository dogRepo;
 
-    @RequestMapping("dogHome")
+    @Autowired
+    private TrainerRepository trainerRepo;
+
+    // Redirect root to dogHome
+    @GetMapping("/")
+    public ModelAndView defaultRoute() {
+        return new ModelAndView("redirect:/dogHome");
+    }
+
+    // Show homepage
+    @GetMapping("/dogHome")
     public ModelAndView home() {
-        ModelAndView mv = new ModelAndView("home");
+        return new ModelAndView("home");
+    }
+
+    // Show form to add new dog
+    @GetMapping("/add")
+    public ModelAndView addDogForm() {
+        ModelAndView mv = new ModelAndView("addNewDog");
+        mv.addObject("trainers", trainerRepo.findAll());
         return mv;
     }
 
-    @RequestMapping("add")
-    public ModelAndView add() {
-        ModelAndView mv = new ModelAndView("addNewDog.html");
-        Iterable<Trainer> trainerList = trainerRepo.findAll();
-        mv.addObject("trainers", trainerList);
-        return mv;
-    }
-
-    @RequestMapping("addNewDog")
+    // Process new dog addition
+    @PostMapping("/addNewDog")
     public ModelAndView addNewDog(Dog dog, @RequestParam("trainerId") int id) {
         Trainer trainer = trainerRepo.findById(id).orElse(null);
         if (trainer != null) {
             dog.setTrainer(trainer);
             dogRepo.save(dog);
         }
-        ModelAndView mv = new ModelAndView("home");
-        return mv;
+        return new ModelAndView("redirect:/dogHome");
     }
 
-    @RequestMapping("viewModifyDelete")
-    public ModelAndView viewDogs() {
+    // View all dogs
+    @GetMapping("/viewModifyDelete")
+    public ModelAndView viewAllDogs() {
         ModelAndView mv = new ModelAndView("viewDogs");
-        Iterable<Dog> dogList = dogRepo.findAll();
-        mv.addObject("dogs", dogList);
+        mv.addObject("dogs", dogRepo.findAll());
         return mv;
     }
 
-    @RequestMapping("editDog")
+    // Edit an existing dog
+    @PostMapping("/editDog")
     public ModelAndView editDog(Dog dog) {
         dogRepo.save(dog);
-        ModelAndView mv = new ModelAndView("editDog");
-        return mv;
+        return new ModelAndView("redirect:/viewModifyDelete");
     }
 
-    @RequestMapping("deleteDog")
+    // Delete dog by id
+    @GetMapping("/deleteDog")
     public ModelAndView deleteDog(@RequestParam("id") int id) {
+        dogRepo.findById(id).ifPresent(dogRepo::delete);
+        return new ModelAndView("redirect:/viewModifyDelete");
+    }
+
+    // Search for a dog by ID
+    @GetMapping("/search")
+    public ModelAndView searchDogById(@RequestParam("id") int id) {
+        ModelAndView mv = new ModelAndView();
         Dog dog = dogRepo.findById(id).orElse(null);
         if (dog != null) {
-            dogRepo.delete(dog);
-        }
-        return home();
-    }
-
-    @RequestMapping("search")
-    public ModelAndView searchById(@RequestParam("id") int id) {
-        ModelAndView mv = new ModelAndView();
-        Dog dogFound = dogRepo.findById(id).orElse(null);
-        if (dogFound != null) {
-            mv.addObject("dog", dogFound);
             mv.setViewName("searchresults");
+            mv.addObject("dog", dog);
         } else {
+            mv.setViewName("error");
             mv.addObject("message", "Dog not found!");
-            mv.setViewName("error"); // Use an error view or display a friendly message on the home page.
         }
         return mv;
     }
 
-    @RequestMapping("addTrainer")
-    public ModelAndView addTrainer() {
-        ModelAndView mv = new ModelAndView("addNewTrainer.html");
-        return mv;
+    // Show form to add new trainer
+    @GetMapping("/addTrainer")
+    public ModelAndView addTrainerForm() {
+        return new ModelAndView("addNewTrainer");
     }
 
-    @RequestMapping("trainerAdded")
-    public ModelAndView addNewTrainer(Trainer trainer) {
+    // Process trainer addition
+    @PostMapping("/trainerAdded")
+    public ModelAndView trainerAdded(Trainer trainer) {
         trainerRepo.save(trainer);
-        ModelAndView mv = new ModelAndView("home");
-        return mv;
+        return new ModelAndView("redirect:/add");
     }
 }
